3f22d977c6504637499fcaa263428013d1ee651a
xkawixgirlx <jennkosh@gmail.com>
2024-05-10 15:01:18 -0700

    Add Placeholder image to HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,8 @@ export default function HomePage({ user }) {
     return (
        <>
        <h2>Welcome! Click the button below!</h2>
+       <img src='https://cdn.pixabay.com/photo/2022/06/21/21/15/audio-7276511_1280.jpg' />
+       <br />
        <Link to='/auth'>Enter! </Link>
        </> 
     )

76505de83f1d53a9d93a3f5b5d0c2999c2eebd6e
xkawixgirlx <jennkosh@gmail.com>
2024-05-10 09:51:13 -0700

    Project Base Refactor, remove unused components and change App.jsx

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,8 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
@@ -12,18 +10,14 @@ export default function App() {
 
   return (
     <main className="App">
-      { user ?
-          <>
-            <NavBar user={user} setUser={setUser} />
-            <Routes>
-              {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
-              <Route path="/orders" element={<OrderHistoryPage />} />
-            </Routes>
-          </>
-          :
-          <AuthPage setUser={setUser} />
-      }
+      <NavBar user={user} setUser={setUser} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/posts" element={<PostsPage />} />
+        {/* additional Routes... */}
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
     </main>
   );
 }
--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -9,6 +9,3 @@ export async function login(credentials) {
   return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
 }
 
-export async function checkToken() {
-  return sendRequest(`${BASE_URL}/check-token`);
-}

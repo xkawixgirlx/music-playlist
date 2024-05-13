0007c750db17f68231bca91316ead752a7fb23a8
xkawixgirlx <jennkosh@gmail.com>
2024-05-13 12:53:57 -0700

    Add newVideo for Playlist form

--- src/components/PlayListForm/PlayListForm.jsx
+++ src/components/PlayListForm/PlayListForm.jsx
@@ -2,9 +2,48 @@ import { useState } from 'react';
 
 
 export default function PlayListForm({ handleAddPlaylist }) {
+    const [newVideo, setNewVideo] = useState({
+        title: '',
+        artist: '',
+        videoUrl: '',
+    });
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        handleAddPlaylist();
+        setNewVideo(newVideo);
+    }
+
     return (
-<>
-<h2>PlayListForm</h2>
-</>
+        <>
+            <h2>Make a PlayList/Add Tracks</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Youtube URL: </label>
+                <input
+                    name='videoUrl'
+                    value={newVideo.videoUrl}
+                    onChange={() => setNewVideo(newVideo)}
+                    required
+                    pattern='.{2, }'
+                />
+                <label>Artist Name: </label>
+                <input
+                    name='artist'
+                    value={newVideo.artist}
+                    onChange={() => setNewVideo(newVideo)}
+                    required
+                    pattern='.{1, }'
+                />
+                <label>Track Title:  </label>
+                <input
+                    name='title'
+                    value={newVideo.title}
+                    onChange={() => setNewVideo(newVideo)}
+                    required
+                    pattern='.{2, }'
+                />
+                <button type='submit'>Make Playlist/AddTrack</button>
+            </form>
+        </>
     );
 }

e42cd8c5fa1caef404346b980718010b64573ae7
xkawixgirlx <jennkosh@gmail.com>
2024-05-13 09:06:49 -0700

    Refactor NavBar Component for Dynamic Render

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,24 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
 
 export default function NavBar({ setUser }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   function handleLogOut() {
     userService.logOut();
     setUser(null);
+    setIsLoggedIn(isLoggedIn);
   }
 
   return (
     <nav>
       <Link to='/playlist'>Playlist</Link> 
       &nbsp;&nbsp;
-      <Link to="" onClick={handleLogOut}>Log Out</Link>
+      {isLoggedIn ? (
+        <Link to='/auth'>Login/Signup</Link>
+      ) : (
+        <Link to="" onClick={handleLogOut}>Log Out</Link>
+      )}
     </nav>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function HomePage() {
     return (
         <>
-            <h2>Welcome! Click the button below!</h2>
+            <h2>Welcome! Please click Enter!</h2>
             <img src='https://cdn.pixabay.com/photo/2022/06/21/21/15/audio-7276511_1280.jpg' alt='headphones' />
             <br />
             <Link to='/auth'>Enter! </Link>

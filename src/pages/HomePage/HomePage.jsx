79fd9e55be0794603b6906c1dc0b07240fb4f14c
xkawixgirlx <jennkosh@gmail.com>
2024-05-16 12:57:46 -0700

    favicon change

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
             <h2>Welcome! Please click Enter!</h2>
             <img src='https://cdn.pixabay.com/photo/2022/06/21/21/15/audio-7276511_1280.jpg' alt='headphones' />
             <br />
-            <button><Link to='/auth'>Enter! </Link></button>
+            <button class='enter-btn'><Link to='/auth'>Enter! </Link></button>
         </>
     )
 }
